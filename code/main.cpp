0cf443fbdea1546bc3635a77fcea6932f894d887
leopold-lll <stefano.leonardi-2@studenti.unitn.it>
2018-10-25 11:33:26 +0200

    add control on sides before apply the rotation

--- code/main.cpp
+++ code/main.cpp
@@ -24,7 +24,7 @@ static const int LIMIT    = W_0 + 4*SIZE + 3*W_OFFSET;
 int W_now = W_0;
 int H_now = H_0;
 
-const string filename = "../data/map/03.jpg";
+const string filename = "../data/map/02.jpg";
 const string calib_file = "../support_file/intrinsic_calibration.xml";
 
 // support function
@@ -35,7 +35,7 @@ float distance(Point c1, Point c2);
 // sorting point function
 void PrintPoints(const char *caption, const vector<Point_<int> > & points);
 double Orientation(const Point &a, const Point &b, const Point &c);
-void Sort4PointsClockwise(vector<Point_<int> > & points);
+void Sort4PointsCounterClockwise(vector<Point_<int> > & points);
 double CrossProductZ(const Point &a, const Point &b);
 
 int main(){
@@ -68,7 +68,7 @@ int main(){
     // Find black regions (filter on saturation and value)
     // HSV range opencv: Hue range is [0,179], Saturation range is [0,255] and Value range is [0,255]
     Mat black_mask;
-    inRange(hsv_img, Scalar(0, 0, 0), Scalar(180, 255, 50), black_mask);  
+    inRange(hsv_img, Scalar(0, 0, 0), Scalar(180, 255, 100), black_mask);  
     my_imshow("BLACK_filter", black_mask);
     
     // Find contours
@@ -80,7 +80,7 @@ int main(){
     findContours(black_mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE); // find external contours of each blob
 
     //cout << "N. contours: " << contours.size() << endl;
-    double area, area_max = 0;
+    double area, area_max = -1000;
     //for series of points
     for (int i=0; i<contours.size(); ++i){
         approxPolyDP(contours[i], approx_polygon, 10, true); // fit a closed polygon (with less vertices) to the given contour, with an approximation accuracy (i.e. maximum distance between the original and the approximated curve) of 3
@@ -100,18 +100,20 @@ int main(){
         }
     }
     cout << "area max: " << area << endl;
-
+    
     // show the quadrilateral found
     Mat quadrilateral_img = fix_img.clone();
     drawContours(quadrilateral_img, contours_approx_big, -1, Scalar(0,170,220), 5, LINE_AA);
     my_imshow("Find rectangle", quadrilateral_img);
     
-    //sort 
+    //sort of 4 points in counterclockwise order
     PrintPoints("input: ", rect);
-    Sort4PointsClockwise(rect);
+    Sort4PointsCounterClockwise(rect);
     PrintPoints("output: ", rect);
     printf("\n");
-    /*int min_x=100000, min_index=0;
+    
+    //sort in clockwise order starting from the leftmost corner
+    int min_x=100000, min_index=0;
     for(int i=0; i<4; i++){
         if(rect[i].x < min_x){
             min_x = rect[i].x;
@@ -139,7 +141,23 @@ int main(){
             swap(rect[1], rect[2]); 
         break;
     }
-    PrintPoints("output: ", rect);*/
+    PrintPoints("output: ", rect);
+
+    //check that the first side is the shortest
+    float avg0 = ( distance(rect[0],rect[1]) + distance(rect[2],rect[3])/2.5 )/2;
+    float avg1 = ( distance(rect[1],rect[2]) + distance(rect[3],rect[4])/2.5 )/2;
+    if(distance(rect[0],rect[1])>distance(rect[1],rect[2])){  //far  sides
+    //if(distance(rect[2],rect[3])>distance(rect[3],rect[4])){//near sides
+        cout << "\navg0 > avg1\n";
+        Point tmp = rect[0];
+        for(int i=0; i<3; i++){
+            rect[i] = rect[i+1];
+        }
+        rect[3] = tmp;
+    }else{
+        cout << "\navg0 < avg1\n";
+    }
+    PrintPoints("output: ", rect);
 
     // wrap the perspective
     Mat corner_pixels = (Mat_<float>(4,2) << rect[0].x, rect[0].y, rect[1].x, rect[1].y, rect[2].x, rect[2].y, rect[3].x, rect[3].y);
@@ -214,7 +232,7 @@ double Orientation(const Point &a, const Point &b, const Point &c) {
     return CrossProductZ(a, b) + CrossProductZ(b, c) + CrossProductZ(c, a);
 }
 
-void Sort4PointsClockwise(vector<Point_<int> > & points){
+void Sort4PointsCounterClockwise(vector<Point_<int> > & points){
     Point& a = points[0];
     Point& b = points[1];
     Point& c = points[2];
