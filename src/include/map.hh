f09dbbe0751d733c887ea9c9a09fe76c83abf58e
leopold-lll <stefano.leonardi-2@studenti.unitn.it>
2019-07-12 12:29:32 +0200

    add header of Map

--- src/include/map.hh
+++ src/include/map.hh
@@ -1,4 +1,34 @@
 #ifndef MAP
 #define MAP
 
+#include <vector>
+#include <iostream>
+
+#include "maths.hh"
+
+class Map{
+    public:
+        typedef short int bit;  // it can be chenged to bool
+
+        Map(int lengthX=1000, int lengthY=1500, int xx=5, int yy=5,
+            vector< vector<Point2<int> > > vvp = vector< vector<Point2<int> > >() );
+
+        void addObject(vector<Point2<int> > vp);
+        void printMatrix();
+        string toString();
+
+        bool checkPoint(Point2<int> p);
+        bool checkSegment(Point2<int> p1, Point2<int> p2);
+
+    protected:
+        bit **map;
+
+        int lengthX;    // dimension of the arena default: 1000
+        int lengthY;    // dimension of the arena d: 1500
+        int dimX;       // how many cells in the map: 200
+        int dimY;       // how many cells in the map: 300
+        int xx;         // dimension of the cell (pixels): 5
+        int yy;         // dimension of the cell (pixels): 5
+};
+
 #endif
--- test/map_main.cc
+++ test/map_main.cc
@@ -1,7 +1,29 @@
-#include <map.hh>
+//#include <map.hh>
 #include <iostream>
+#include <bitset>         // std::bitset
+
+using namespace std;
+
+void test(int xx, int yy){
+    int **map = new int*[xx];
+    for(int i = 0; i < xx; i++) {
+        map[i] = new int[yy];
+    }
+    map[1][2] = 5;
+    for(int i=0; i<xx; i++){
+        for(int j=0; j<yy; j++){
+            cout << map[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
 int main(){
-    std::cout << "pippo\n";
-    
+    cout << "MAIN MAP\n";
+    test(3, 5);
+    int a = 5;
+    short int b = 3;
+    bool c = 1;
+    cout << sizeof(a) << sizeof(b) << sizeof(c) << endl;
 }
