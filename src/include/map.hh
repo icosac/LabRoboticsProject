7778014fc5b418f7e2d111ff480367044d94a6de
leopold-lll <stefano.leonardi-2@studenti.unitn.it>
2019-07-12 13:14:00 +0200

    basic function implemented

--- src/include/map.hh
+++ src/include/map.hh
@@ -8,20 +8,22 @@
 
 class Map{
     public:
-        typedef short int bit;  // it can be chenged to bool
+        typedef short int myBit;  // it can be chenged to bool
 
-        Map(int lengthX=1000, int lengthY=1500, int xx=5, int yy=5,
+        Map(int _lengthX=1000, int _lengthY=1500, int _xx=5, int _yy=5,
             vector< vector<Point2<int> > > vvp = vector< vector<Point2<int> > >() );
 
         void addObject(vector<Point2<int> > vp);
-        void printMatrix();
-        string toString();
+
+        void printMap();
+        string matrixToString();
+        void printDimensions();
 
         bool checkPoint(Point2<int> p);
         bool checkSegment(Point2<int> p1, Point2<int> p2);
 
     protected:
-        bit **map;
+        myBit **map;
 
         int lengthX;    // dimension of the arena default: 1000
         int lengthY;    // dimension of the arena d: 1500
--- src/map.cc
+++ src/map.cc
@@ -1,1 +1,60 @@
 #include <map.hh>
+
+/*
+bool checkPoint(Point2<int> p);
+bool checkSegment(Point2<int> p1, Point2<int> p2);
+*/
+
+Map::Map(int _lengthX=1000, int _lengthY=1500, int _xx=5, int _yy=5,
+    vector< vector<Point2<int> > > vvp = vector< vector<Point2<int> > >() ){
+    
+    lengthX = _lengthX;
+    lengthY = _lengthY;
+    xx = _xx;
+    yy = _yy;
+    
+    dimX = int(ceil(lengthX*1.0 / xx));
+    dimY = int(ceil(lengthY*1.0 / yy));
+    map = new myBit*[dimX];
+    for(int i=0; i<dimX; i++){
+        map[i] = new myBit[dimY];
+    }
+
+    printDimensions();
+    printMap();
+
+    for(int i=0; i<vvp.size(); i++){
+        addObject(vvp[i]);
+    }
+    printMap();
+}
+
+void Map::addObject(vector<Point2<int> > vp){
+    for(int i = 0; i<vp.size(); i++) cout << vp[i] << endl;
+}
+
+void Map::printDimensions(){
+    cout << "lengthX: " << lengthX << ", lengthY: " << lengthY << endl;
+    cout << "dimX: " << dimX << ", dimY: " << dimY << endl;
+    cout << "xx: " << xx << ", yy: " << yy << endl;
+}
+
+string Map::matrixToString(){
+    stringstream ss;
+    
+    for(int i=0; i<dimX; i++){
+        for(int j=0; j<dimY; j++){
+            ss << map[i][j] << " ";
+        }
+        ss << endl;
+    }
+    ss << endl;
+
+    string s = ss.str();
+    return(s);
+}
+
+void Map::printMap(){
+    printDimensions();
+    cout << matrixToString();
+}
